cb32c671af9b235459e294b954915e05ca88b4ae
Md Rehanullah <157832474+Md-Rehanullah@users.noreply.github.com>
2025-06-22 09:24:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.getElementById("date").innerHTML = `<h4>${day}, ${mname} ${date}, ${yea
 
 // Fetching the news from the API
 
-const apiKey = 'pub_479521869e790a727903df673ac804ca5f7dc';
+const apiKey = 'pub_479521869e790a727903df673ac804ca5f7';
 let country = 'in';
 const maxNews = 12;
 const loadMoreCount = 6;
